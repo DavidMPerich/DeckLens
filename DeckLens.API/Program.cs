c0fd3193a7eacaa5e9bd5643218f36f01d69ff57
DavidMPerich <davidmperich@gmail.com>
2026-02-08 15:43:28 -0700

    consume scryfall api test

--- DeckLens.API/Program.cs
+++ DeckLens.API/Program.cs
@@ -1,3 +1,6 @@
+using DeckLens.API.Services.Implementation;
+using DeckLens.API.Services.Interface;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ICardService, CardService>();
+
+builder.Services.AddHttpClient<ICardService, CardService>(client =>
+{
+    client.BaseAddress = new Uri("https://api.scryfall.com");
+    client.DefaultRequestHeaders.Add("User-Agent", "DeckLens/1.0");
+});
 
 var app = builder.Build();
 
