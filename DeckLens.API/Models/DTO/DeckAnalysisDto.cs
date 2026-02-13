0d3a63e52f0d140c1fd66881df2ca00799fc702c
DavidMPerich <davidmperich@gmail.com>
2026-02-13 10:39:31 -0700

    build metric calculator stub

--- DeckLens.API/Models/DTO/DeckAnalysisDto.cs
+++ DeckLens.API/Models/DTO/DeckAnalysisDto.cs
@@ -1,6 +1,14 @@
-﻿namespace DeckLens.API.Models.DTO
+﻿using StackExchange.Redis;
+
+namespace DeckLens.API.Models.DTO
 {
     public class DeckAnalysisDto
     {
+        public CardDto? Commander {  get; set; }
+        public int TotalCards { get; set; }
+        public double AverageCmc { get; set; }
+        public Dictionary<int, int> ManaCurve { get; set; } = new();
+        public Dictionary<string, int> ColorDistribution { get; set; } = new();
+        public Dictionary<string, int> CardTypeBreakdown { get; set; } = new();
     }
 }
--- DeckLens.API/Services/Implementation/DeckMetricCalculator.cs
+++ DeckLens.API/Services/Implementation/DeckMetricCalculator.cs
@@ -1,11 +1,41 @@
 ﻿using DeckLens.API.Models.DTO;
 using DeckLens.API.Services.Interface;
+using System.Reflection.Metadata.Ecma335;
 
 namespace DeckLens.API.Services.Implementation
 {
     public class DeckMetricCalculator : IDeckMetricCalculator
     {
         public DeckAnalysisDto Build(List<CardDto> cards)
+        {
+            var dto = new DeckAnalysisDto();
+
+            dto.TotalCards = cards.Count;
+
+            return dto;
+        }
+
+        private double CalculateAverageCmc(List<CardDto> cards) 
+        { 
+            throw new NotImplementedException();
+        }
+
+        private Dictionary<int, int> BuildManaCurve(List<CardDto> cards) 
+        {
+            throw new NotImplementedException();
+        }
+
+        private Dictionary<string, int> BuildColorDistribution(List<CardDto> cards)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Dictionary<string, int> BuildTypeBreakdown(List<CardDto> cards)
+        {
+            throw new NotImplementedException();
+        }
+
+        private CardDto? DetectCommander(List<CardDto> cards)
         {
             throw new NotImplementedException();
         }
